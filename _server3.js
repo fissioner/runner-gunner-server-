40b43f44685a29d1a4bfd172f69e20b076606e45
Derek <26776606+fissioner@users.noreply.github.com>
2019-01-04 19:11:23 -0700

    Update Server

--- _server3.js
+++ _server3.js
@@ -1,6 +1,4 @@
-require('dotenv').config();
-const express = require('express'),
-sms = require('twilio')(process.env.TWILIO_ID, process.env.TWILIO_TOKEN),
+const express = require('express');
 app = express(),
     http = require('http').Server(app),
     io = require('socket.io')(http),
@@ -13,10 +11,8 @@ let users = [],
     players = [],
     bullets = [],
     enemies = [],
-    bg = [{type: 'bg', x: 0}, {type: 'bg', x: 750}],
     lives,
     gravity = 0.5,
-    soloGame = false,
     random = function (min, max) {
         let num = Math.floor(Math.random() * Math.floor(max));
         return num > min ? num : num + min;
@@ -27,14 +23,6 @@ let users = [],
         width: 750
     },
     loop;
-
-const { Client } = require('pg'),
-  client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
 
 function element(x, y, width, height, color, type, name, emoji) {
     this.name = name || '';
@@ -51,58 +39,55 @@ function element(x, y, width, height, color, type, name, emoji) {
         left: false,
         right: false,
         up: false,
-        sounds: {
-            jump: false,
-            shoot: false
-        }
     };
 }
 
-    function createPlatforms() {
-        for (let i = 1; i < 10; i++) {
-            let width = random(50, 400),
-                height = random(50, 400),
-                color = colors[random(0, colors.length)],
-                x = platforms[platforms.length - 1].x + random(50, 200) + platforms[platforms.length - 1].width;
-            platforms.push(new element(x, c.height - height, width, height, color, 'platform'));
-            if (random(0, 10) < 6) {
-                function makeEnemy() {
-                    enemies.push(new element(x + random(10, width - 25), platforms[platforms.length - 1].y - 45, 25, 45, 'green', 'enemy'));
-                    if (random(0, 10) < 5) makeEnemy();
-                }
-                makeEnemy();
-            }
-        }
-    }
-
 function startGame() {
     loop = setInterval(makeElements, 20);
-    lives = 1;
+    lives = 2;
     score = 0;
     enemies.push(new element(0, 0, 0, 0, 'gray', 'bullet'));
 
-    //create initial platforms & enemies
+    //create initial platforms
 
     platforms.push(new element(100, c.height - 50, 100, 50, 'green', 'platform'));
-    createPlatforms();
+    for (let i = 1; i < 500; i++) {
+        let width = random(50, 400),
+            height = random(50, 400),
+            color = colors[random(0, colors.length)],
+            x = platforms[i - 1].x + random(50, 200) + platforms[i - 1].width;
+        platforms.push(new element(x, c.height - height, width, height, color, 'platform'));
+    }
+
+    //create enemies
+
+    platforms.forEach(p => {
+        if (p !== platforms[0] && random(0, 10) < 5) {
+            enemies.push(new element(p.x + random(10, p.width - 30), p.y - 45, 25, 45, 'green', 'enemy'));
+        }
+        function enemy() {
+            if (p !== platforms[0] && random(0, 10) < 4) {
+                enemies.push(new element(p.x + random(10, p.width - 30), p.y - 45, 25, 45, 'green', 'enemy'));
+                enemy();
+            }
+        }
+        enemy();
+    })
 };
 
 function makeElements() {
     let floor = c.height + 100;
-    if (platforms.length < 10) {
-        createPlatforms();
-        //createEnemies(6);
-    }
 
     if (platforms[0].x + platforms[0].width < 0) {
         platforms.shift();
     }
-    if (enemies[1] && enemies[1].x + 40 < 0) {
+    if (enemies[1].x + 40 < 0) {
         enemies.splice(1, 1);
     }
     players.forEach(p => {
         if (p.y > c.height) {
             lives -= 1;
+            console.log(lives);
         }
         p.yVelocity += gravity;
         p.y += p.yVelocity;
@@ -114,12 +99,9 @@ function makeElements() {
         }
         if (p.controls.right) {
             p.x += 5;
+
         }
         if (p.x > c.width - 200) {
-            bg.forEach(bg => {
-                if (bg.x < -750) bg.x = 745
-                bg.x -= 5
-            });
             enemies.forEach(e => {
                 e.x -= 5;
             })
@@ -133,18 +115,14 @@ function makeElements() {
                 lives -= 1;
             }
         })
-
-        if (p.x > c.width - 200) {
-            platforms.forEach(plat => {
-            plat.x -= 5;
-            });
-            p.x -= 5;
-            if (!soloGame) {
-            let otherPlayer = players.filter(pl => pl !== p);
-            otherPlayer[0].x -= 5;
-            }
-        }
         platforms.forEach(plat => {
+            if (p.x > c.width - 200) {
+                plat.x -= 5;
+                p.x -= .01;
+                let otherPlayer = players.filter(pl => pl !== p);
+                otherPlayer[0].x -= .01;
+            }
+
             if (p.x >= plat.x && p.x <= plat.x + plat.width) {
                 floor = c.height - 40 - plat.height;
                 if (p.y > floor && p.y < floor + 50) {
@@ -154,45 +132,34 @@ function makeElements() {
             }
         })
     })
-    io.emit('drawElements', [bg, players, platforms, enemies, bullets]);
+
+    io.emit('drawElements', [players, platforms.slice(0, 6), enemies.slice(1, 10), bullets]);
+
 
     if (lives < 1 || players.length === 0) {
             io.emit('gameOver');
             io.emit('stopGame', false);
             clearInterval(loop);
-
-            if (players.length > 0) {
-            client.query(`insert into scores values(default, '${players[0].name}', ${score}, default)`, (err, res) => {
-                if (err) throw err;
-              });
-
-            client.query(`select * from scores order by score desc limit 25`, (err, res) => {
-                if (err) throw err;
-                io.emit('showScores', JSON.stringify(res.rows));
-              });
-            }
             platforms = [];
             bullets = [];
             enemies = [];
             players = [];
-            soloGame = false;
-            console.log('Game Over');
+            console.log('Game Over', lives);
         }
 
-
     for (let i = 0; i < bullets.length; i++) {
-        bullets[i].x += 8;
+        bullets[i].x += 5;
         for (let j = 0; j < enemies.length; j++) {
             if (bullets[i].x > enemies[j].x && bullets[i].x < enemies[j].x + enemies[j].width &&
                 bullets[i].y > enemies[j].y && bullets[i].y < enemies[j].y + enemies[j].height) {
-                enemies.splice(j, 1);
+                enemies.splice(j, j);
                 bullets[i].x = c.width;
                 score += 1;
                 io.emit('score', score);
             }
         };
-        if (bullets[i].x > c.width) {
-            bullets.splice(i, 1);
+        if (bullets[i].x > bullets[i].x + c.width) {
+            bullets[i].splice(i, i);
         }
     }
 }
@@ -200,19 +167,9 @@ function makeElements() {
 
 io.on('connection', function (socket) {
     console.log(`New User Connection, Socket ID: ${socket.id}`);
-    /*sms.messages.create({
-        to: process.env.TWILIO_TO,
-        from: process.env.TWILIO_FROM,
-        body: 'A user connected to Runner Gunner'
-    })*/
     let user = UsernameGenerator.generateUsername("-");
     user = user.split('-');
     user = user[1] + '-' + user[0];
-
-    client.query(`select * from scores order by score desc limit 25`, (err, res) => {
-        if (err) throw err;
-        io.emit('showScores', JSON.stringify(res.rows));
-      });
 
     users.push(user);
     socket.emit('user', user);
@@ -233,13 +190,13 @@ io.on('connection', function (socket) {
     });
 
     socket.on('controls', function (c) {
-        if (players.length === 2 || soloGame) {
+        if (players.length === 2) {
             let player = players.filter(p => p.name === c.userName);
             player[0].controls = c.controls;
         }
     });
     socket.on('bullets', function (userName) {
-        if (players.length === 2 || soloGame) {
+        if (players.length === 2) {
         let player = players.filter(p => p.name === userName);
         bullets.push(new element(player[0].x + 20, player[0].y + 10, 10, 10, 'gray', 'bullet'));
         }
@@ -257,14 +214,12 @@ io.on('connection', function (socket) {
             console.log('Game in Progress');
         }
     });
-    socket.on('soloGame', function (userName) {
-        soloGame = true;
-        socket.emit('joinedGame');
-        players.push(new element(100 + (100 / 2) - (20 / 2), c.height - 50 - 40, 20, 40, 'orange', 'player', userName, emojis[random(0, 2440)]));
+    socket.on('startSolo', function () {
         io.emit('startGame', true);
-        startGame();
-        console.log('Game in Progress');
-    })
+    });
+    socket.on('stopSolo', function () {
+        io.emit('stopGame', false);
+    });
 });
 
 const port = process.env.PORT || 5000;
